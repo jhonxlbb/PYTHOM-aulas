829bd6ea093b4d3745de3d1122c8c6c3e9f726cd
jhonxlbb <wtgsduzzff@gmail.com>
2024-08-16 18:14:13 +0000

    comandos iniciais

---
+++ 1.Tipos primitivos /Tipagem dinamica.py
@@ -0,0 +1,7 @@
+nome = "jhones"
+idade = 22
+altura = 1.71
+
+print(f"Nome:  {nome}")
+print(f"Idade:  {idade}")
+print(f"Altura:  {altura}")
---
+++ 1.Tipos primitivos /Tipagem estatica.py
@@ -0,0 +1,15 @@
+import os
+
+os.system("cls || clear") # limpa o terminal
+#tipagem estática
+nome : str # Equivalente a Cadeia
+idade : int # Equivalente a inteiro
+altura : float # Equivalente a real 
+
+nome = "Marta"
+idade = 17
+altura = 1.71
+
+print(f"Nome: {nome}")
+print(f"Idade: {idade}")
+print(f"Altura: {altura}") 
