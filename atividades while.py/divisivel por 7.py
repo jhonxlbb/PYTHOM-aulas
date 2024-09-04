90e6b49eeb9bd4978c5be348352505f271446260
jhonxlbb <wtgsduzzff@gmail.com>
2024-09-04 16:24:43 +0000

    atividade

---
+++ atividades while.py/divisivel por 7.py
@@ -0,0 +1,12 @@
+import os 
+os.system("cls || clear")
+
+# Começa com o número 51, pois precisamos encontrar um número maior que 50
+numero = 51
+
+# Laço while para encontrar o número divisível por 7
+while numero % 7 != 0:
+    numero += 1  # Add o número até encontrar um que seja divisível por 7
+
+# Exibe o número encontrado
+print(f"O primeiro número maior que 50 que é divisível por 7 é: {numero}")
